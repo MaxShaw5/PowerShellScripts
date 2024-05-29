edb923a0da7c94bda736a02208e8e96b920682fe
MaxShawAzure <shawmax55@gmail.com>
2024-05-29 15:01:08 -0500

    Add files via upload
    The Jabber Script PS script is used when Jabber is not showing a phone number in the bottom left of the application and is showing a red icon.

---
+++ Jabber Script.ps1
@@ -0,0 +1,9 @@
+﻿Stop-process -Name 'CiscoJabber'
+Start-Sleep -Seconds 3
+$CurrentUser = [Environment]::UserName
+Remove-Item -Path "C:\Users\$CurrentUser\appdata\Local\Cisco\Unified Communications", "C:\Users\$CurrentUser\AppData\Roaming\Cisco\Unified Communications" -Force -Recurse
+ipconfig /flushdns
+Start-Process -FilePath 'C:\Program Files (x86)\Cisco Systems\Cisco Jabber\CiscoJabber.exe'
+Write-Host "Process Completed"
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
---
+++ Printer Quick Add.ps1
@@ -0,0 +1,6 @@
+﻿$printername = Read-Host "Enter printer name"
+add-printer -connectionname \\miusprint\$printername
+Write-Host "Complete!"
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+
