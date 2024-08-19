39aad52459d387cc64234b152f5cc9c5f3403964
Max Shaw <shawmax55@gmail.com>
2024-08-19 08:50:07 -0500

    Printer Quick Add.ps1

--- Printer Quick Add.ps1
+++ Printer Quick Add.ps1
@@ -1,6 +1,13 @@
-﻿$printername = Read-Host "Enter printer name"
-add-printer -connectionname \\miusprint\$printername
+#Initialize a variable that will read input from the user 
+
+$printername = Read-Host "Enter printer name"
+
+#Use the cmdlet to add a printer and pass in the name of the printer as gathered from the Read-Host cmdlet above. "NameOfPrintServer" will of course be swapped to whatever print server you are looking to add the printer from.
+
+add-printer -connectionname \\NameOfPrintServer\$printername
+
+#Inform the user that the printer has been added and prompt them to enter any key to close the window
+
 Write-Host "Complete!"
 Write-Host -NoNewLine 'Press any key to continue...';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
-
